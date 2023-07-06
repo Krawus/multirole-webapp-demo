628e39a2919ab2bbf19add64ee0873609fa6e0ef
Krawus <krawczykadam21@gmail.com>
2023-07-06 20:28:55 +0200

    user log in and logout

---
+++ src/main/java/com/krawus/multirole_website/security/SecurityConfiguration.java
@@ -0,0 +1,58 @@
+package com.krawus.multirole_website.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService usersSetup() {
+        UserDetails user = User.builder()
+            .username("user")
+            .password("{noop}user")
+            .roles("USER")
+            .build();
+
+        UserDetails moderator = User.builder()
+            .username("moderator")
+            .password("{noop}moderator")
+            .roles("USER", "MOD")
+            .build();
+
+        UserDetails admin = User.builder()
+            .username("admin")
+            .password("{noop}admin")
+            .roles("USER", "MOD", "ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, moderator, admin);
+    }
+
+
+    @Bean 
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(authorize -> 
+            authorize
+                .anyRequest().authenticated()
+        )
+        .formLogin(form -> 
+            form
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticateUser")
+                .permitAll()
+        )
+        .logout(logout ->
+                    logout.permitAll()
+        );
+        
+
+        return http.build();
+    }
+}
